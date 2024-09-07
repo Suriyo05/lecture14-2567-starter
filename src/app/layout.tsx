cf1d4172ecb94e7ae31806dfc2f65584d1a4154b
Suriyo05 <suriyo_lumsom@cmu.ac.th>
2024-09-07 11:33:07 +0700

    lec14 fi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css"
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MantineProvider>
+          {children}
+
+        </MantineProvider>
+        </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,73 @@
+"use client"
+import { Modal,Button ,Title,TextInput,Group,Container, Select,Radio,Checkbox,Text,Anchor, Space} from "@mantine/core";
+import { runningPlans } from "@components/libs/runningPlans";
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
 export default function Home() {
-  return <div>Home Page</div>;
+
+  const [opened, {open,close}] = useDisclosure(false);
+  const [termAccepted,setTermAccepted] = useState(false);
+  return (
+    <>
+
+    <Container size="500px">
+  <Title fs="italic" ta="center">Register CMU Marathon</Title>
+  
+  <Group grow>
+  <TextInput
+      label="First Mame"
+      placeholder="Input First Name"
+      required
+    />
+    <TextInput
+      label="Last Mame"
+      placeholder="Input Last Name"
+      required
+    />
+  </Group>
+  <Select
+    label="Running Plan"
+    data={runningPlans}
+    placeholder="Select Running Plan"
+    searchable={true}
+    required
+  />
+
+  <Space h="lg"/>
+<Radio.Group label="Gender" >
+<Radio mt="sm" labelPosition="right" label="Male" color="teal"
+    />
+<Radio mt="sm" labelPosition="right" label="Female" color="teal"
+    />
+</Radio.Group>
+
+
+<Space h="lg"/>
+
+<Group>
+<Checkbox checked={termAccepted}
+onChange={(event)=> setTermAccepted(event.target.checked)}/>
+
+
+<Text>I accept <Anchor onClick={open} >terms and conditions</Anchor>
+  </Text>
+  
+</Group>
+<Group grow>
+<Button mt="sm" onClick={open} disabled={!termAccepted}>Register</Button>
+
+</Group>
+</Container>
+
+<Modal opened={opened} onClose={close} title="Terms and conditions">
+  <Text>
+    1.First terms and conditions...<br/>
+    2.Second terms and conditions...<br/>
+    3.Third terms and conditions...<br/>
+  </Text>
+  <Button onClick={close}>Confirm</Button>
+</Modal>
+
+    </>
+  );
 }
